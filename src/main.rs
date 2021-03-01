0765bf6bfa09364e914f150bc4345ffea902ede3
doramatadora <hello@doramilitaru.com>
2021-03-01 11:04:09 +0000

    implement flow change suggestion

--- src/main.rs
+++ src/main.rs
@@ -17,10 +17,66 @@ fn main(mut req: Request) -> Result<Response, Error> {
     // Load the service configuration, and the OpenID discovery and token signature metadata.
     let settings = Config::load();
 
-    // Verify any tokens stored as a result of a complete OAuth 2.0 authorization code flow.
+    // Parse the Cookie header.
     let cookie_header = req.remove_header_str("cookie").unwrap_or_default();
     let cookie = cookies::parse(&cookie_header);
 
+    // Build the OAuth 2.0 redirect URL.
+    let redirect_uri = format!(
+        "https://{}{}",
+        req.get_url().host_str().unwrap(),
+        settings.config.callback_path
+    );
+
+    // If the path matches the redirect URL path, continue the OAuth 2.0 authorization code flow.
+    if req.get_url_str().starts_with(&redirect_uri) {
+        // VERIFY THE AUTHORIZATION CODE AND EXCHANGE IT FOR TOKENS.
+
+        // Retrieve the code and state from the query string.
+        let qs: CallbackQueryParameters = req.get_query().unwrap();
+        return match (cookie.get("state"), cookie.get("code_verifier")) {
+            (Some(state), Some(code_verifier))
+                if base64::encode_config(state, base64::URL_SAFE_NO_PAD) == qs.state =>
+            {
+                // Exchange the authorization code for tokens.
+                let mut exchange_res = Request::post(settings.openid_configuration.token_endpoint)
+                    .with_body_form(&ExchangePayload {
+                        client_id: &settings.config.client_id,
+                        client_secret: settings.config.client_secret,
+                        code: &qs.code,
+                        code_verifier: code_verifier,
+                        grant_type: "authorization_code",
+                        redirect_uri: &redirect_uri,
+                    })
+                    .unwrap()
+                    .send("idp")?;
+                // If the exchange is successful, proceed with the original request.
+                if exchange_res.get_status().is_success() {
+                    // Strip the random state from the state cookie value to get the original request.
+                    let original_req =
+                        &state[..(state.len() - settings.config.state_parameter_length)];
+                    // Deserialize the response from the authorize step.
+                    let auth = exchange_res.take_body_json::<AuthorizeResponse>().unwrap();
+                    // Replay the original request, setting the tokens as cookies.
+                    Ok(responses::temporary_redirect(
+                        original_req,
+                        cookies::permanent("access_token", &auth.access_token, auth.expires_in),
+                        cookies::permanent("id_token", &auth.id_token, auth.expires_in),
+                        cookies::expired("code_verifier"),
+                        cookies::expired("state"),
+                    ))
+                // Otherwise, surface any errors from the Identity Provider.
+                } else {
+                    Ok(responses::unauthorized(exchange_res.take_body()))
+                }
+            }
+            _ => Ok(responses::unauthorized(
+                "State parameter mismatch. Try again...",
+            )),
+        };
+    }
+
+    // Verify any tokens stored as a result of a complete OAuth 2.0 authorization code flow.
     if let (Some(access_token), Some(id_token)) =
         (cookie.get("access_token"), cookie.get("id_token"))
     {
@@ -64,94 +120,36 @@ fn main(mut req: Request) -> Result<Response, Error> {
         return Ok(req.send("backend")?);
     }
 
-    // Start or continue the OAuth 2.0 authorization code flow.
-
-    // Build the OAuth 2.0 redirect URL.
-    let redirect_uri = format!(
-        "https://{}{}",
-        req.get_url().host_str().unwrap(),
-        settings.config.callback_path
-    );
-
-    // If the path matches the redirect URL path, continue the OAuth 2.0 authorization code flow.
-    if req.get_url_str().starts_with(&redirect_uri) {
-        // VERIFY THE AUTHORIZATION CODE AND EXCHANGE IT FOR TOKENS.
-
-        // Retrieve the code and state from the query string.
-        let qs: CallbackQueryParameters = req.get_query().unwrap();
-        match (cookie.get("state"), cookie.get("code_verifier")) {
-            (Some(state), Some(code_verifier))
-                if base64::encode_config(state, base64::URL_SAFE_NO_PAD) == qs.state =>
-            {
-                // Exchange the authorization code for tokens.
-                let mut exchange_res = Request::post(settings.openid_configuration.token_endpoint)
-                    .with_body_form(&ExchangePayload {
-                        client_id: &settings.config.client_id,
-                        client_secret: settings.config.client_secret,
-                        code: &qs.code,
-                        code_verifier: code_verifier,
-                        grant_type: "authorization_code",
-                        redirect_uri: &redirect_uri,
-                    })
-                    .unwrap()
-                    .send("idp")?;
-                // If the exchange is successful, proceed with the original request.
-                if exchange_res.get_status().is_success() {
-                    // Strip the random state from the state cookie value to get the original request.
-                    let original_req =
-                        &state[..(state.len() - settings.config.state_parameter_length)];
-                    // Deserialize the response from the authorize step.
-                    let auth = exchange_res.take_body_json::<AuthorizeResponse>().unwrap();
-                    // Replay the original request, setting the tokens as cookies.
-                    Ok(responses::temporary_redirect(
-                        original_req,
-                        cookies::permanent("access_token", &auth.access_token, auth.expires_in),
-                        cookies::permanent("id_token", &auth.id_token, auth.expires_in),
-                        cookies::expired("code_verifier"),
-                        cookies::expired("state"),
-                    ))
-                // Otherwise, surface any errors from the Identity Provider.
-                } else {
-                    Ok(responses::unauthorized(exchange_res.take_body()))
-                }
-            }
-            _ => Ok(responses::unauthorized(
-                "State parameter mismatch. Try again...",
-            )),
-        }
     // Otherwise, start the OAuth 2.0 authorization code flow.
-    } else {
-        // REQUEST AN AUTHORIZATION CODE.
 
-        // Generate the Proof Key for Code Exchange (PKCE) code verifier and code challenge.
-        let pkce = Pkce::new(&settings.config.code_challenge_method);
-        // Generate the Oauth 2.0 state parameter,
-        // adding a nonce to the original request URL to prevent attacks and redirect users.
-        let state = format!(
-            "{}{}{}",
-            req.get_path(),
-            req.get_query_str().unwrap_or(""),
-            rand_chars(settings.config.state_parameter_length)
-        );
-        // Build the authorization request.
-        let authorize_req = Request::get(settings.openid_configuration.authorization_endpoint)
-            .with_query(&AuthCodePayload {
-                client_id: &settings.config.client_id,
-                code_challenge: &pkce.code_challenge,
-                code_challenge_method: &settings.config.code_challenge_method,
-                redirect_uri: &redirect_uri,
-                response_type: "code",
-                scope: "openid",
-                state: &base64::encode_config(&state, base64::URL_SAFE_NO_PAD),
-            })
-            .unwrap();
-        // Redirect to the Identity Provider's login and authorization prompt.
-        Ok(responses::temporary_redirect(
-            authorize_req.get_url_str(),
-            cookies::expired("access_token"),
-            cookies::expired("id_token"),
-            cookies::session("code_verifier", &pkce.code_verifier),
-            cookies::session("state", &state),
-        ))
-    }
+    // Generate the Proof Key for Code Exchange (PKCE) code verifier and code challenge.
+    let pkce = Pkce::new(&settings.config.code_challenge_method);
+    // Generate the Oauth 2.0 state parameter,
+    // adding a nonce to the original request URL to prevent attacks and redirect users.
+    let state = format!(
+        "{}{}{}",
+        req.get_path(),
+        req.get_query_str().unwrap_or(""),
+        rand_chars(settings.config.state_parameter_length)
+    );
+    // Build the authorization request.
+    let authorize_req = Request::get(settings.openid_configuration.authorization_endpoint)
+        .with_query(&AuthCodePayload {
+            client_id: &settings.config.client_id,
+            code_challenge: &pkce.code_challenge,
+            code_challenge_method: &settings.config.code_challenge_method,
+            redirect_uri: &redirect_uri,
+            response_type: "code",
+            scope: "openid",
+            state: &base64::encode_config(&state, base64::URL_SAFE_NO_PAD),
+        })
+        .unwrap();
+    // Redirect to the Identity Provider's login and authorization prompt.
+    Ok(responses::temporary_redirect(
+        authorize_req.get_url_str(),
+        cookies::expired("access_token"),
+        cookies::expired("id_token"),
+        cookies::session("code_verifier", &pkce.code_verifier),
+        cookies::session("state", &state),
+    ))
 }

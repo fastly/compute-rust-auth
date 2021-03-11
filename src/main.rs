102700cab800e8b445492999bf099dbd3969d8ec
doramatadora <hello@doramilitaru.com>
2021-03-11 14:02:08 +0000

    add hmac checks for state and nonce

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use fastly::{Error, Request, Response};
 use idp::{AuthCodePayload, AuthorizeResponse, CallbackQueryParameters, ExchangePayload};
 use jwt::validate_token_rs256;
 use pkce::{rand_chars, Pkce};
+use hmac_sha256::HMAC;
+use std::str;
 
 #[fastly::main]
 fn main(mut req: Request) -> Result<Response, Error> {
@@ -34,10 +36,21 @@ fn main(mut req: Request) -> Result<Response, Error> {
 
         // Retrieve the code and state from the query string.
         let qs: CallbackQueryParameters = req.get_query().unwrap();
+        // Verify that the state matches what we've stored, and exchange the authorization code for tokens.
         return match (cookie.get("state"), cookie.get("code_verifier")) {
-            (Some(state), Some(code_verifier))
-                if base64::encode_config(state, base64::URL_SAFE_NO_PAD) == qs.state =>
-            {
+            (Some(state), Some(code_verifier)) => {
+                // Decode the state query string parameter and retrieve the nonce.
+                let decoded_state_bytes = base64::decode_config(qs.state, base64::URL_SAFE_NO_PAD)?;
+                let decoded_state = str::from_utf8(&decoded_state_bytes).unwrap();
+                let nonce =
+                    &decoded_state[..(decoded_state.len() - settings.config.state_parameter_length)];
+                let hashed_state_bytes = HMAC::mac(&decoded_state_bytes, &nonce.as_bytes());
+                // Compare the state cookie to the hashed query string state.
+                if state.as_bytes() != hashed_state_bytes {
+                    return Ok(responses::unauthorized(
+                        "State parameter mismatch. Try again...",
+                    ));
+                }
                 // Exchange the authorization code for tokens.
                 let mut exchange_res = Request::post(settings.openid_configuration.token_endpoint)
                     .with_body_form(&ExchangePayload {
@@ -54,7 +67,7 @@ fn main(mut req: Request) -> Result<Response, Error> {
                 if exchange_res.get_status().is_success() {
                     // Strip the random state from the state cookie value to get the original request.
                     let original_req =
-                        &state[..(state.len() - settings.config.state_parameter_length)];
+                        &decoded_state[..(decoded_state.len() - settings.config.state_parameter_length)];
                     // Deserialize the response from the authorize step.
                     let auth = exchange_res.take_body_json::<AuthorizeResponse>().unwrap();
                     // Replay the original request, setting the tokens as cookies.
@@ -125,13 +138,13 @@ fn main(mut req: Request) -> Result<Response, Error> {
     let pkce = Pkce::new(&settings.config.code_challenge_method);
     // Generate a random value (nonce) to mitigate OAuth replay attacks. 
     let nonce = rand_chars(settings.config.state_parameter_length);
-    // Generate the Oauth 2.0 state parameter,
-    // adding a nonce to the original request URL to prevent attacks and redirect users.
+    // Generate the Oauth 2.0 state parameter, used to prevent CSRF attacks,
+    // by adding the nonce and a random string to the original request URL.
     let state = format!(
         "{}{}{}",
         req.get_path(),
         req.get_query_str().unwrap_or(""),
-        rand_chars(settings.config.state_parameter_length)
+        &nonce
     );
     // Build the authorization request.
     let authorize_req = Request::get(settings.openid_configuration.authorization_endpoint)
@@ -146,12 +159,15 @@ fn main(mut req: Request) -> Result<Response, Error> {
             nonce: Some(&nonce)
         })
         .unwrap();
+    // Encrypt the state using the nonce as hash.
+    // We'll use this HMAC to store the state into a cookie.
+    let hashed_state_bytes = HMAC::mac(&state.as_bytes(),&nonce.as_bytes());
     // Redirect to the Identity Provider's login and authorization prompt.
     Ok(responses::temporary_redirect(
         authorize_req.get_url_str(),
         cookies::expired("access_token"),
         cookies::expired("id_token"),
         cookies::session("code_verifier", &pkce.code_verifier),
-        cookies::session("state", &state),
+        cookies::session("state", str::from_utf8(&hashed_state_bytes).unwrap())
     ))
 }

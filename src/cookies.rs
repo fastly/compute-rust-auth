9c547cc96fe4accc5a53124964501a4cb4d552e4
Dora Militaru <dora@kalneus.com>
2024-07-03 11:31:35 +0100

    apply feedback from review

--- src/cookies.rs
+++ src/cookies.rs
@@ -2,8 +2,15 @@ use std::collections::HashMap;
 
 const COOKIE_ATTRIBUTES: &str = "Path=/; SameSite=Lax; Secure; HttpOnly";
 
-// Change to "__Secure-" for production, and "local-" for local development.
-const COOKIE_PREFIX: &str = "local-";
+// Returns the "__Secure-" cookie prefix in production, and "local-" in development.
+pub fn get_cookie_prefix() -> &'static str {
+    let fastly_service_version = std::env::var("FASTLY_SERVICE_VERSION").unwrap_or("0".to_string());
+    if fastly_service_version == "0" {
+        "local-"
+    } else {
+        "__Secure-"
+    }
+}
 
 pub fn parse(cookie_string: &str) -> HashMap<&str, &str> {
     cookie_string
@@ -11,7 +18,7 @@ pub fn parse(cookie_string: &str) -> HashMap<&str, &str> {
         .filter_map(|kv| {
             kv.find('=').map(|index| {
                 let (key, value) = kv.split_at(index);
-                let key = key.trim().trim_start_matches(COOKIE_PREFIX);
+                let key = key.trim().trim_start_matches(get_cookie_prefix());
                 let value = value[1..].trim();
                 (key, value)
             })
@@ -22,7 +29,11 @@ pub fn parse(cookie_string: &str) -> HashMap<&str, &str> {
 pub fn persistent(name: &str, value: &str, max_age: u32) -> String {
     format!(
         "{}{}={}; Max-Age={}; {}",
-        COOKIE_PREFIX, name, value, max_age, COOKIE_ATTRIBUTES
+        get_cookie_prefix(),
+        name,
+        value,
+        max_age,
+        COOKIE_ATTRIBUTES
     )
 }
 
@@ -31,5 +42,11 @@ pub fn expired(name: &str) -> String {
 }
 
 pub fn session(name: &str, value: &str) -> String {
-    format!("{}{}={}; {}", COOKIE_PREFIX, name, value, COOKIE_ATTRIBUTES)
+    format!(
+        "{}{}={}; {}",
+        get_cookie_prefix(),
+        name,
+        value,
+        COOKIE_ATTRIBUTES
+    )
 }
